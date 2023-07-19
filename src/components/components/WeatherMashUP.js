1f38925d0a26250f4e41f91eed4807b03dd52ee9
kudakwashedelanesibanda <kudakwashedelanesibanda@gmail.com>
2023-07-19 14:40:17 +0200

    Renamed the javascript file name from HomePage.js to SearchPage.js

--- src/components/components/WeatherMashUP.js
+++ src/components/components/WeatherMashUP.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Alert, Card, CardImg, Col, Container } from "react-bootstrap";
 import axios from "axios";
-import SearchPage from "./HomePage";
+import SearchPage from "./SearchPage";
+
 
 const CLIENT_ID = '327d46894e804b639c22cad035f65300'; // Replace with your actual Spotify client ID
 const CLIENT_SECRET = '3626ccee911b49209110b815652c1b4f'; // Replace with your actual Spotify client secret
